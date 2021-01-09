305d4874dea6a5e2a7bec55a89289abe170c0068
xjiminkimx <kjim217@gmail.com>
2021-01-10 01:55:45 +0900

    #4 Room App

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,96 @@
+# From Django
 from django.db import models
 
+# Third Party Packages
+from django_countries.fields import CountryField
+
+# MyApps
+from core import models as core_models
+from users import models as user_models
+
+
 # Create your models here.
+
+
+class AbstractItem(core_models.TimeStampedModel):
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """ Room Type """
+
+    class Meta:
+        verbose_name_plural = "Room Type"
+        ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+
+    """ Room Type """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """ House Rule """
+
+    class Meta:
+        verbose_name_plural = "House Rule"
+
+
+class Room(core_models.TimeStampedModel):
+
+    """ Room Model Difinition """
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    geusts = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenity = models.ManyToManyField(Amenity, blank=True)
+    fAcility = models.ManyToManyField(Facility, blank=True)
+    house_rule = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
